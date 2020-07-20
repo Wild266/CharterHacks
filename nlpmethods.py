cc9a92aa40b25a236c5a3dea57b26410f8f0a1a2
Wild266 <32823036+Wild266@users.noreply.github.com>
2020-07-20 19:36:30 -0400

    Update nlpmethods.py

--- nlpmethods.py
+++ nlpmethods.py
@@ -2,6 +2,54 @@ import spacy
 from spacy.matcher import Matcher
 nlp = spacy.load('en_core_web_sm')
 matcher = Matcher(nlp.vocab)
+
+MY_FNAME = "SARA"
+MY_LNAME = "SARA"
+
+def respond(text, transcript, sentiment):
+   text = text.lower()
+   transcript.append(text)
+   matcher = Matcher(nlp.vocab)
+   doc = nlp(text)
+   response = ""
+   namedentities = give_namedentities(doc)
+   print(namedentities)
+   if len(namedentities)>0:
+      response += f"Does {namedentities[0]} interest you?"
+      return response
+
+
+#   full_name = extract_full_name(doc)
+   basin = give_base(doc)
+   subjects = give_subjects(doc)
+   directobjs = give_directobj(doc)
+   roots = give_ROOT(doc)
+#    nc = give_nounchunks(doc)
+   if len(subjects)>=1:
+      return f"Oh! I'm a chatbot so I don't know what a {subjects[0]} is. Could you tell me about it? Please"
+   if len(directobjs) >=1:
+      return f"Tell me more about {directobjs[0]}"
+   if text == "yes":
+      return "Great! I can't wait"
+   if text == "no":
+      return "why not?"
+   if "your name" in text or "SARA" in text:
+         return "My name is SARA"
+
+   
+   if nlp("do") in roots:
+      if "die" in basin:
+         response = "do you want to talk about it?"
+         return response
+      if sentiment == "POSITIVE":
+         return "You make me feel good :)"
+   response = "Interesting, would you like to tell me more?"
+   return response
+
+
+
+
+
 def extract_full_name(nlp_doc):
     pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
     matcher.add('FULL_NAME', None, pattern)
@@ -21,22 +69,22 @@ def give_subjects(nlp_doc):
    s = []
    for token in nlp_doc:
       if token.dep_ == "nsubj":
-         s.append(token.dep_)
-      print (token.text, token.tag_, token.head.text, token.dep_)
+         s.append(token.text)
+      #print (token.text, token.tag_, token.head.text, token.dep_)
    return s
 def give_directobj(nlp_doc):
    s = []
    for token in nlp_doc:
       if token.dep_ == "dobj":
-         s.append(token.dep_)
-      print (token.text, token.tag_, token.head.text, token.dep_)
+         s.append(token.text)
+      #print (token.text, token.tag_, token.head.text, token.dep_)
    return s
 def give_ROOT(nlp_doc):
    s = []
    for token in nlp_doc:
       if token.dep_ == "ROOT":
          s.append(token)
-      print (token.text, token.tag_, token.head.text, token.dep_)
+      #print (token.text, token.tag_, token.head.text, token.dep_)
    return s
 def give_nounchunks(nlp_doc):
    return nlp_doc.noun_chunks
